0559dc6bea7ca84095c9bd7ee481b8974476c5e4
sarishmachinta <sarishmachintha@gmail.com>
2026-03-31 11:24:58 +0530

    Add files via upload

---
+++ perimeter python.py
@@ -0,0 +1,5 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> side=int(input())
+... area= side*side
+... perimeter=4*side

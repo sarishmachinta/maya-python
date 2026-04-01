6fa162d07dd4cad2b313ce6a685931b51ebbc034
sarishmachinta <sarishmachintha@gmail.com>
2026-04-01 07:18:01 +0530

    Add files via upload

---
+++ colour code.py
@@ -0,0 +1,17 @@
+ch = input().strip().upper()
+if ch == 'V':
+    print("Violet")
+elif ch == 'I':
+    print("Indigo")
+elif ch == 'B':
+    print("Blue")
+elif ch == 'G':
+    print("Green")
+elif ch == 'Y':
+    print("Yellow")
+elif ch == 'O':
+    print("Orange")
+elif ch == 'R':
+    print("Red")
+else:
+    print(-1)
